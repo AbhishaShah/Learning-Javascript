a0afb8ffb664b7e2488fc90ff5dcc2029997c559
Abhisha Shah <abhisha5993@gmail.com>
2021-09-12 22:35:13 -0400

    Learned about modules

---
+++ Modules/module1.js
@@ -0,0 +1,12 @@
+const firstname = "Abhi";
+const lastname = "Shah";
+
+// export specific named values using export keyword
+export {firstname, lastname};
+
+// inline export
+export const country = "Canada";
+
+// default export, using default keyword
+const language = "Javascript"; 
+export default language;
---
+++ Modules/module2.js
@@ -0,0 +1,4 @@
+// import the default value and three named values from module1.js
+import knownLanguage, {firstname,lastname,country} from "./module1.js";
+
+console.log(`${firstname} ${lastname} works on ${knownLanguage}`); // Abhi Shah works on Javascript
